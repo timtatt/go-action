09f8584a304ba62208af5e930a63fcf81b0b3f9c
Tim Tattersall <tim@tattsy.com>
2025-08-04 13:53:18 +1000

    fix: update shim

--- dist/shim.js
+++ dist/shim.js
@@ -1,3 +1,17 @@
-const { exec } = require("node:child_process");
+const { spawn } = require("child_process");
+const path = require("path");
 
-exec(__dirname + "/action");
+// Resolve the binary path (relative to this script)
+const binaryPath = path.resolve(__dirname, "./action");
+
+// Forward CLI args
+const args = process.argv.slice(2);
+
+// Spawn the binary
+const proc = spawn(binaryPath, args, {
+  stdio: "inherit",
+});
+
+proc.on("exit", (code) => {
+  process.exit(code);
+});
